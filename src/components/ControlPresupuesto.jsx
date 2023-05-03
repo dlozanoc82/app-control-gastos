9e1c29eec6640662d116d97be1474e1a7085c5a6
Daniel <dlozanoc862@gmail.com>
2023-05-03 18:30:31 -0500

    [ADD] Grafica del porcentaje gastado del presupuesto inicial

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,16 +1,27 @@
 import { useState, useEffect } from "react";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const ControlPresupuesto = ({presupuesto, gastos}) => {
 
+    const [porcentaje, setPorcentaje] = useState(0);
     const [disponible, setDisponible] = useState(0);
     const [gastado, setGastado] = useState(0);
 
     useEffect(() => {
         const totalGastado = gastos.reduce((total, gasto) => Number(gasto.cantidad) + total, 0);
         const disponible = presupuesto-totalGastado;
+
+        //Calcular Porcentaje
+        const nuevoPorcentaje = (( (presupuesto-disponible) / presupuesto) * 100).toFixed(2);
         
         setGastado(totalGastado);
         setDisponible(disponible);
+
+        setTimeout(() => {
+            setPorcentaje(nuevoPorcentaje);
+        }, 1000);
+
     }, [gastos])
 
 
@@ -25,7 +36,15 @@ const ControlPresupuesto = ({presupuesto, gastos}) => {
     return (
         <div className="contenedor-presupuesto contenedor sombra dos-columnas">
             <div>
-                <p>Grafica aqui</p>
+                <CircularProgressbar 
+                    styles={buildStyles({
+                        pathColor: '#3B82F6',
+                        trailColor: '#F5F5F5',
+                        textColor: '#3B82F6',
+                    })}
+                    value={porcentaje}
+                    text={`${porcentaje}% Gastado`}
+                />
             </div>
             <div className="contenido-presupuesto">
                 <p>
