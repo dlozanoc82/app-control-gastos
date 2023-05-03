ce05ea00332c596055b75605d845967c27f09eb4
Daniel <dlozanoc862@gmail.com>
2023-05-03 11:56:45 -0500

    [ADD] Se agregan los campos del formulario Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -22,6 +22,44 @@ const Modal = ({setModal, animarModal, setAnimarModal}) => {
             </div>
             <form className={`formulario ${animarModal ? "animar": "cerrar"}`}>
                 <legend>Nuevo Gasto</legend>
+
+                <div className='campo'>
+                    <label htmlFor="nombre">Nombre Gasto</label>
+                    <input 
+                        id='nombre'
+                        type="text"
+                        placeholder="Añade el nombre del gasto"
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="cantidad">Cantidad</label>
+                    <input 
+                        id='cantidad'
+                        type="text"
+                        placeholder="Añade el cantidad del gasto"
+                    />
+                </div>
+
+                <div className='campo'>
+                    <label htmlFor="categoria">Categoria</label>
+                    <select 
+                        name="" 
+                        id="categoria"
+                    >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorro">Ahorro</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos Varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="">Suscripcione</option>
+                    </select>
+                </div>
+
+                <input type="submit" value='Añadir Gasto' />
+
             </form>
         </div>
     )
